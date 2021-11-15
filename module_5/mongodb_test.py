7b3bc478837795ba66b943a8f09ac96ea434ee8b
johnny <jonathan.kobyluck@gmail.com>
2021-11-14 23:30:08 -0500

    .

---
+++ module_5/mongodb_test.py
@@ -0,0 +1,7 @@
+from pymongo import MongoClient
+
+url = "mongodb+srv://admin:admin@cluster0.duvph.mongodb.net/Cluster0?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
+client = MongoClient(url)
+db = client.pytech
+
+print(db.list_collection_names)
