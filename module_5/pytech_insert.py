f32b9e76ec1c22c672e2f64a324b0c54468795b7
johnny <jonathan.kobyluck@gmail.com>
2021-11-21 22:36:35 -0500

    module 5.3

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -1,3 +1,25 @@
+from pymongo import MongoClient
+
+url = "mongodb+srv://admin:admin@cluster0.duvph.mongodb.net/Cluster0?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
+client = MongoClient(url)
+db = client.pytech
+students = db["students"]
+
+john = {
+    "student_id" : 1007,
+    "first_name" : "John" 
+}
+
+jane = {
+    "student_id" : 1008,
+    "first_name" : "Jane"
+}
+
+josh = {
+    "student_id" : 1009,
+    "first_name" : "Josh"
+}
+
 john_student_id = students.insert_one(john).inserted_id
 jane_student_id = students.insert_one(jane).inserted_id
 josh_student_id = students.insert_one(josh).inserted_id
---
+++ module_5/pytech_queries.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient
+
+url = "mongodb+srv://admin:admin@cluster0.duvph.mongodb.net/Cluster0?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
+client = MongoClient(url)
+db = client.pytech
+students = db["students"]
+
+# for docs in students.find():
+#     print(docs)
+
+for docs in students.find_one({}, {"student_id": 1007}):
+    print(docs)
